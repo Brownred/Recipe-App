4f94490b0d62ab73b7f973b041659bf5506973a2
Lenox <lenoxmiheso@gmail.com>
2024-04-21 23:30:34 +0300

    modified:   Backend/src/index.ts 	new file:   Backend/src/recipe-api.ts

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -2,7 +2,7 @@
 
 import express from 'express'
 import cors from 'cors'
-import e from 'express';
+import searchRecipes from './recipe-api.ts'
 
 // create a new express app
 const app = express(); // this is the app that we will use to create our server. an analogy is that it is like a factory that creates a car
@@ -13,8 +13,7 @@ app.use(cors()); // this is a middleware that allows us to make requests from ou
 // an end point is like a door that you knock on to get a response.the function is like the person that opens the door when you knock on the door.the request is like the knock on the door
 app.get("/api/recipes/search/", async (req, res) => {
     // the code that will handle the request and response will go here
-    res.json({message: "Hello World!"}); // return this success message whenever this endpoint is called
-
+    const results = searchRecipes();
 }) // this is the endpoint that we will use to test that our server is working. an analogy is that it is like a test drive. the first parameter is a string: the end  point that this function is going to handle; an analogy is that it is like the address of a house. 
 // the second parameter is a function that is going to handle the request and response. an analogy is that it is like the person that is going to open the door when you knock on the door. 
 
---
+++ Backend/src/recipe-api.ts
@@ -0,0 +1,30 @@
+
+export const searchRecipes = async (searchTerm: string, page: number) => {
+    const apiKey = process.env.API_KEY;
+    
+    if (!apiKey) {
+        throw new Error('API key not found');
+    }
+
+    const url = new URL('https://api.spoonacular.com/recipes/complexSearch'); // create a new url object that will help make things like parameters easier
+    // now create the query params like api keys, search terms, etc
+
+    const queryParams = {
+        apiKey: apiKey,
+        query: searchTerm,
+        number: "10", // howmany messages we want to get per request
+        offset: ((page - 1) * 10).toString(), // this is how we can get the next page of 
+    };
+    url.search = new URLSearchParams(queryParams).toString(); // this will convert the query params into a string that can be appended to the url
+
+    try {
+        const searchResponse = await fetch(url);
+        const resultJson = await searchResponse.json();
+        return resultJson;
+    } catch (error) {
+        console.error('Error fetching recipes', error);
+        return null;
+    }
+};
+
+export default searchRecipes;
