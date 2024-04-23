e3f9cb5e3da8f9eff7ce55b2621a784a3188bf9f
Lenox <lenoxmiheso@gmail.com>
2024-04-23 21:43:54 +0300

    modified:   Frontend/index.html modified:   Frontend/src/App.tsx new file:   Frontend/src/api.ts modified:   Frontend/src/main.tsx new file:   Frontend/src/types.ts

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,22 +1,45 @@
 import "./App.css";
-import { useState } from "react";
-
-
+import { FormEvent, useState } from "react";
+import searchRecipes from "./api";
+import { Recipe } from "./types";
 
 
 function App() {
 
-  const [searchTerm, setSearchTerm] = useState("fries");
-  const [recipes, setRecipes] = useState([]);
+  const [searchTerm, setSearchTerm] = useState<string>("fries");
+  const [recipes, setRecipes] = useState<Recipe[]>([]);
 
-  const handleSearchSubmit = async () => {
-    
-  }
+  const handleSearchSubmit = async (event: FormEvent) => {
+    event.preventDefault();
+    try {
+      const recipes = await searchRecipes(searchTerm, 1);
+      setRecipes(recipes.results);
+    } catch(e) {
+      console.log(e);
+    }
+  };
 
   return (
-    <>
-      <h1>Recipe App</h1>
-    </>
+      <div className="">
+        <form action="" onSubmit={(event)=> handleSearchSubmit(event)}>
+          <input type="text"
+          required
+          placeholder="Enter a cerch term ..."
+          value={searchTerm}
+          onChange={(event) => setSearchTerm(event.target.value)}
+           />
+          <button type="submit">
+            Submit
+          </button>
+        </form>
+        
+        {recipes.map((recipe) => (
+          <div key={recipe.id}>
+            recipes image location: {recipe.image}
+            recipes title: {recipe.title}
+          </div>
+        ))}
+      </div>
   )
 }
 
