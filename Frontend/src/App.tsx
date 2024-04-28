5adba441e603c3035c37941c3aef09c04f9e1320
Lenox <lenoxmiheso@gmail.com>
2024-04-28 19:44:35 +0300

    modified:   Frontend/src/App.css 	modified:   Frontend/src/App.tsx

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,18 +1,32 @@
 import "./App.css";
-import { FormEvent, useRef, useState } from "react";
+import { FormEvent, useEffect, useRef, useState } from "react";
 import { searchRecipes } from "./api";
 import { Recipe } from "./types";
 import RecipeCard from "./components/RecipeCard";
 import RecipeModal from "./components/recipeModal";
 
+type Tabs = "search" | "favorites";
 
 function App() {
 
   const [searchTerm, setSearchTerm] = useState<string>("fries");
   const [recipes, setRecipes] = useState<Recipe[]>([]);
   const [selectedRecipe, setSelectedRecipe] = useState<Recipe | null>(null);
+  const [selectedTab, setSelectedTab] = useState<Tabs>("search");
 
   const pageNumber = useRef(1);
+
+  useEffect(()=> {
+    const fetchFavoriteRecipes = async () => {
+      try {
+        const favoriteRecipes = await getFavoriteRecipes();
+      } catch(e) {
+        console.log(e);
+      }
+    };
+
+    fetchFavoriteRecipes();
+  }, [])
 
   const handleSearchSubmit = async (event: FormEvent) => {
     event.preventDefault();
@@ -38,26 +52,38 @@ function App() {
 
   return (
       <div className="">
+        <div className="tabs">
+        <h1 onClick={()=> setSelectedTab("search")}>Recipe Search</h1>
+        <h1 onClick={()=> setSelectedTab("favorites")}>Favourites</h1>
+        </div>
+
+        {selectedTab === "search" && (<>
         <form action="" onSubmit={(event) => handleSearchSubmit(event)}>
           <input type="text"
-          required
-          placeholder="Enter a cerch term ..."
-          value={searchTerm}
-          onChange={(event) => setSearchTerm(event.target.value)}
-           />
+            required
+            placeholder="Enter a cerch term ..."
+            value={searchTerm}
+            onChange={(event) => setSearchTerm(event.target.value)}
+          />
           <button type="submit">
             Submit
           </button>
         </form>
-        
+
         {recipes.map((recipe) => (
-          <RecipeCard recipe={recipe} onClick={()=> setSelectedRecipe(recipe)}/>
+          <RecipeCard recipe={recipe} onClick={() => setSelectedRecipe(recipe)} />
         ))}
-        <button 
+        <button
           className="view-more-button"
           onClick={handleViewMoreCllick}>
           view more
-        </button> 
+        </button>
+        </>)} 
+
+      {selectedTab === "favorites" && (<>
+        <h1>Favourites</h1>
+        <p>Coming soon...</p>
+      </>)}
 
         {selectedRecipe ? (
         <RecipeModal id={selectedRecipe.id} onClose={()=> setSelectedRecipe(null)}/>
